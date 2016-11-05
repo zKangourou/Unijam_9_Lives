49ed6bc199ea328422d209aa61b1ba80a68e7896
TiphaineG <tiphaine.giron@gmail.com>
2016-11-05 17:53:42 +0100

    death scripts

---
+++ Assets/Scripts/Trigger_Action/Voiture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voiture : Interractable
+{
+    [SerializeField]
+    TexteController txtController;
+    string death_dial = "exemple_de_cle";
+
+    public override void Interract()
+    {
+        if (!done)
+        {
+            txtController.StartDialogue(death_dial, TexteController.DialogueType.DIE, Player.Death.voiture);
+            player.AddPower(Player.Power.superman);
+            done = true;
+        }
+    }
+}
