e83bc6ba8ddc67c97099ae8dd4808e90bfd710c3
TiphaineG <tiphaine.giron@gmail.com>
2016-11-05 11:55:57 +0100

    fonction die

--- Assets/Scripts/PlayerMvt/Player.cs
+++ Assets/Scripts/PlayerMvt/Player.cs
@@ -2,46 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     private int life;
-    private bool action, go, death_barbecue;
+    private GameObject trigger;
+    private bool action, death_barbecue;
     private List<bool> death_list = new List<bool>();
     //private List<Transform> myList = new List<Transform>();
     //private Dictionary<Deaths, List<Transform>> myDeath;
 
-    void Start () {
+    void Start()
+    {
         life = 9;
         death_barbecue = false;
-        go = false;
         death_list.Add(death_barbecue);
-	}
-	
-	void Update () {
+    }
+
+    void Update()
+    {
         action = Input.GetButtonDown("Action");
-        if (action) { go = true; }
     }
 
     private void LoseOneLife()
     {
         if (this.life != 0)
         {
+            Debug.Log("Lose one life");
             this.life -= 1;
         }
     }
 
-    void OnTrigger2D(Collider2D coll)
+    public void Die()
     {
-        if (go)
-        {
-            Debug.Log("Action");
-            Barbecue barbecue = coll.gameObject.GetComponent<Barbecue>();
-            if (barbecue)
-            {
-                Debug.Log("Le chat brûle et gagne un pouvoir !");
-                LoseOneLife();
-                death_barbecue = true;
-                go = false;
-            }
-        }
+        LoseOneLife();
     }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        trigger = coll.gameObject;
+        Debug.Log("objet collidé " + trigger.tag);
+        /*
+        if (barbecue)
+        {
+            Debug.Log("Le chat brûle et gagne un pouvoir !");
+            LoseOneLife();
+            death_barbecue = true;
+        }*/
+    }
+
+
 }
