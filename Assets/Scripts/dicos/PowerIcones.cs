f7458e90041c3dde9792a4eae707593507ddee9f
kangourou <simon.vrana@fromthebard.com>
2016-11-05 16:49:51 +0100

    les images sont prètres à êtres mise pour les picto

--- Assets/Scripts/dicos/PowerIcones.cs
+++ Assets/Scripts/dicos/PowerIcones.cs
@@ -7,12 +7,26 @@ public class PowerIcones : Singleton<PowerIcones>
     Dictionary<Player.Death, Sprite> dico;
     [SerializeField] Sprite none;
     [SerializeField] Sprite chute;
-    //[SerializeField] Sprite none;
+    [SerializeField] Sprite ecrase_actif;
+    [SerializeField] Sprite electro_actif;
+    [SerializeField] Sprite electro_passif;
+    [SerializeField] Sprite fire_actif;
+    [SerializeField] Sprite fire_passif;
+    [SerializeField] Sprite noyade_actif;
+    [SerializeField] Sprite noyade_passif;
+    [SerializeField] Sprite strangle_passif;
 
     void Awake()
     {
         dico = new Dictionary<Player.Death, Sprite>();
         dico.Add(Player.Death.chute, chute);
+        dico.Add(Player.Death.voiture, ecrase_actif);
+        dico.Add(Player.Death.tazer, electro_actif);
+        dico.Add(Player.Death.lanceFlamme, fire_actif);
+        dico.Add(Player.Death.barbecue, fire_passif);
+        dico.Add(Player.Death.noyadeActive, noyade_actif);
+        dico.Add(Player.Death.noyadePassive, noyade_passif);
+        dico.Add(Player.Death.pendaison, strangle_passif);
     }
 
 
