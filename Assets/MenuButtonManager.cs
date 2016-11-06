f269ea53e78ea22175ed727be5342bf69d524d53
kangourou <simon.vrana@fromthebard.com>
2016-11-06 14:18:49 +0100

    Ajout scene début

---
+++ Assets/MenuButtonManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour {
+
+	public void Load()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
