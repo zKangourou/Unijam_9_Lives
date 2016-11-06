2fe2f0bc1df1d8e4512ea20167b4461c869a38b1
JPatate <nais.guccerelli@gmail.com>
2016-11-06 13:30:45 +0100

    JE TIRE AVEC LE CUL

---
+++ Assets/Scripts/Piou1.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piou1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(15,0,0) * Time.deltaTime;
+        //GetComponent<Rigidbody2D>().velocity = new Vector3(15,0,0);
+    }
+
+    void OnTriggerEnter2D(Collider2D caisse)
+    {
+        if(caisse.tag == "Player")
+        {
+            return;
+        }
+        if (caisse.tag == "Caisse")
+        {
+            Debug.Log("coucou");
+            Debug.Log("pouf");
+            GameObject.Destroy(caisse.gameObject);
+        }
+        GameObject.Destroy(this.gameObject);
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D caisse)
+    {
+        if (caisse.gameObject.tag == "Player")
+        {
+            return;
+        }
+        if (caisse.gameObject.tag == "Caisse")
+        {
+            Debug.Log("coucou");
+            Debug.Log("pouf");
+            GameObject.Destroy(caisse.gameObject);
+        }
+        GameObject.Destroy(this.gameObject);
+
+    }
+}
--- Assets/Scripts/Trigger_Action/Shoot.cs
+++ Assets/Scripts/Trigger_Action/Shoot.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Shoot : MonoBehaviour {
 
     public GameObject laser;
+    public GameObject laser2;
     public bool canShoot;
     private Player player;
     private bool tir;
@@ -11,17 +12,29 @@ public class Shoot : MonoBehaviour {
     // Use this for initialization
     void Start () {
         player = this.GetComponent<Player>();
-        canShoot = false;
+        canShoot = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Debug.Log(GetComponent<Rigidbody2D>().velocity);
+        Debug.Log(GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMvt>().avance);
         if (!player.isTalking)
         {
             tir = Input.GetKeyDown(KeyCode.Alpha1);
             if (tir && canShoot)
             {
-                Instantiate(laser, player.transform.position, new Quaternion(0,0,0,0));
+
+                if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMvt>().avance > 0)
+                {
+                    Debug.Log("devant");
+                    Instantiate(laser, player.transform.position, new Quaternion(0, 0, 0, 0));
+                }
+                else
+                {
+                    Debug.Log("derrière");
+                    Instantiate(laser2, player.transform.position, new Quaternion(0, 0, 0, 0));
+                }
             }
         }
     }
