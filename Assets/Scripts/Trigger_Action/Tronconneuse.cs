468f056573a19251d80144d1e436cd94dab45e51
TiphaineG <tiphaine.giron@gmail.com>
2016-11-06 14:28:49 +0100

    hkg kyb

--- Assets/Scripts/Trigger_Action/Tronconneuse.cs
+++ Assets/Scripts/Trigger_Action/Tronconneuse.cs
@@ -6,7 +6,9 @@ public class Tronconneuse : Interractable
     [SerializeField]
     TexteController txtController;
     string death_dial = "exemple_de_cle";
-    public bool lumiere = false;
+    [SerializeField]
+    GameObject bob_tronconneuse;
+    private Vector3 posBob;
 
     public override void Interract()
     {
@@ -14,36 +16,38 @@ public class Tronconneuse : Interractable
 
     void OnTriggerEnter2D(Collider2D val)
     {
-        /*
+        posBob = bob_tronconneuse.transform.position;
         Debug.Log("piece sombre");
         this.gameObject.SetActive(false);
         if (val.tag == "Player" && !done)
         {
             player.isTalking = true;
-            player.Kill(Player.Death.voiture);
-            player.AddPower(Player.Power.passeSousLesPortes);
+            player.Kill(Player.Death.tronconneuse);
+            player.AddPower(Player.Power.desolidarisation);
             done = true;
-            StartCoroutine(bob.Move());
-        }*/
+            StartCoroutine(BobMove());
+        }
     }
 
-    /*
-IEnumerator PrintCar()
-{
-    transform.localScale = new Vector3(0, 0, 1);
-    sprite.color = Color.white;
-    bool sound = true;
-    while (transform.localScale.x < 0.99f)
-    {
-        transform.localScale += new Vector3(0.01f, 0.01f, 0);
-        if (sound && transform.localScale.x > 0.20f)
+    IEnumerator BobMove()
+    {        
+        //transform.localScale = new Vector3(0, 0, 1);
+        //sprite.color = Color.white;
+        bool sound = true;
+        //while (transform.localScale.x < 0.99f)s
+        while (bob_tronconneuse.transform.position.x > player.transform.position.x)
         {
-            SoundManager.PlayBruitage(SoundManager.Bruitages.VOITURE);
-            sound = false;
+            bob_tronconneuse.transform.position += new Vector3(-1.0f, 0, 0);
+            SoundManager.PlayBruitage(SoundManager.Bruitages.VIOLON);
+            /*
+            if (sound && transform.localScale.x > 0.20f)
+            {
+                SoundManager.PlayBruitage(SoundManager.Bruitages.VOITURE);
+                sound = false;
+            }*/
+            yield return new WaitForSeconds(0.01f);
         }
-        yield return new WaitForSeconds(0.01f);
+        //transform.localScale = new Vector3(1, 1, 1);
+        txtController.StartDialogue(death_dial, TexteController.DialogueType.DIE, Player.Death.tronconneuse);
     }
-    transform.localScale = new Vector3(1, 1, 1);
-    txtController.StartDialogue(death_dial, TexteController.DialogueType.DIE, Player.Death.voiture);
-}*/
 }
